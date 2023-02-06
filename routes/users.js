353e18f455bb0e11872a0f33a794facb8eaf7494
sungkuk5420 <sungkuk5420@gmail.com>
2023-02-07 05:17:48 +0900

    경로 수정

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("hello")
+});
 router.get("/auth", (req, res) => {
 
   console.log(req.query)
