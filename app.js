76bb8a12aa8aa1277be7f227b4ff99265b5e79b8
sungkuk5420 <sungkuk5420@gmail.com>
2023-02-08 00:43:08 +0900

    cors 설정 업데이트

--- app.js
+++ app.js
@@ -8,20 +8,7 @@ var cors = require('cors');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const corsOpts = {
-  origin: '*',
-
-  methods: [
-    'GET',
-    'POST',
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
-};
-
-app.use(cors(corsOpts));
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
